4ff22ed88cc681ed4c0bc4ca0a8ecdc9cb84333c
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-14 21:00:15 +0530

    add category, post, user models

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+const {Schema} = mongoose
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true,
+    }, 
+    photo: {
+        type: String,
+        required: false
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    categories: {
+        type: Array,
+        required: false
+    }
+}, {timestamps: true})
+
+const Post = mongoose.model("Post", PostSchema)
+
+module.exports = Post
