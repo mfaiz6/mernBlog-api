d17429ffeac15f654109a4ad58aae465d2f70110
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-15 20:33:27 +0530

    add functionality (using multer package) to upoad images

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+const multer = require("multer")
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI)
 .then(console.log("Connected to MongoDB."))
 .catch((err) => console.log(err))
+
+
+
+// for profile picture upload and store:
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage})
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded.")
+})
+
+
+
 
 app.use("/api/auth", authRoute)
 
@@ -21,6 +43,8 @@ app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
 
 app.use("/api/categories", categoryRoute)
+
+
 
 app.listen(5000, () => {
     console.log("Backend is running.")
