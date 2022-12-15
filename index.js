9da69b9b1adf9263991149cad378608dbed8baa6
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-15 17:39:07 +0530

    add user CRUD ops

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const mongoose = require("mongoose")
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 
 mongoose.set("strictQuery", false);
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err) => console.log(err))
 
 app.use("/api/auth", authRoute)
+
+app.use("/api/users", userRoute)
 
 app.listen(5000, () => {
     console.log("Backend is running.")
--- routes/auth.js
+++ routes/auth.js
@@ -38,9 +38,7 @@ router.post('/login', async (req, res) => {
         const { password, ...others} = user._doc
         res.status(200).json(others);
     } catch (err) {
-
         res.status(500).json(err);
-
     }
 
 });
